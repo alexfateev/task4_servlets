7c62102655278a9707ab14214713888bd8770216
alexfateev <mailboxfav@gmail.com>
2023-11-26 12:31:51 +0500

    di-annotation

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package ru.netology.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
@@ -10,7 +11,7 @@ import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
-// Stub
+@Repository
 public class PostRepository {
 
     private ConcurrentHashMap<Long, Post> repository = new ConcurrentHashMap<>();
